dcd12b0fe99c263138aa113bc93792ec1ebb5ed4
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-14 15:35:22 +0100

    finished our own project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <h1> {{ chartOptions.chart.title }} </h1>
-    <GChart
-      type="ColumnChart"
-      :data="energy_mix"
-      :options="chartOptions"
-    />
-  </div>
+  <div id="main-div">
+    <h1> Vue Google Graphs </h1>
+    <h2> {{ chartOptions.chart.subtitle }} </h2>
+    <div id="piechart-3d">
+        <GChart
+          type="PieChart"
+          :data="energy_mix"
+          :options="chartOptions"
+        />
+    </div>
+    <hr>
+    <populations></populations>
+  </div> <!-- end of main div -->
 </template>
 
 <script>
+import Populations from './components/Populations.vue';
+
 export default {
   name: 'app',
   data: function () {
@@ -18,11 +25,22 @@ export default {
       chartOptions: {
         chart: {
           title: 'Energy Mix',
-          subtitle: 'Yay!',
-        }
+          subtitle: 'Type of Energy vs Percentage of Energy Generated',
+        },
+        is3D: true,
+        chartArea: {
+          left: 0,
+          right: 0,
+          height: 600,
+          width: 400
+        },
+        legend: {
+          right: 0
+        },
+        height: 400,
+        width: "100%"
       }
     }
-  
   },
   mounted: function () {
     fetch('https://api.carbonintensity.org.uk/generation')
@@ -38,9 +56,16 @@ export default {
       this.energy_mix.unshift(['Fuel', 'Percentage']);
     });
   },
+  components: {
+    'populations': Populations
+  }
 }
 </script>
 
 <style>
-
+  #piechart-3d{
+    padding: 40px;
+    border: 1px solid black;
+    width: 50%
+  }
 </style>
---
+++ src/components/Populations.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+
+        <h2>{{chartOptions.chart.title}}</h2>
+        <GChart
+          type="PieChart"
+          :data="populations"
+          :options="chartOptions"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'populations',
+    data: function () {
+        return {
+            populations:[],
+            resultsOfFetch:[],
+            chartOptions: {
+            chart: {
+            title: 'Populations By Country Over 100 Million',
+            subtitle: 'pie',
+            },
+            is3D: true,
+            chartArea: {
+                left: 0,
+                right: 0,
+                height: 600,
+                width: 400
+                },
+                legend: {
+                right: 0
+                },
+                height: 400,
+                width: "50%"
+            }
+        }
+    },
+    mounted: function () {
+            fetch('https://restcountries.eu/rest/v2/all?fields=name;population')
+                .then(res => res.json())
+                .then(data => {
+                    const origArray = data;
+                    let result = [];
+                    for (const object of origArray) {
+                        if (object['population'] > 100000000) {
+                            let array = [object['name'], object['population']];
+                            result.push(array);
+                        }
+                    }
+                    result.unshift(['Country', 'Population']);
+                    this.populations = result;
+                    return this.populations
+                });
+        }
+    }
+</script>
+
+<style>
+
+</style>
