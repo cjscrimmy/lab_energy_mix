db24db600062ecf02ea5a79e4d15ff699165fda4
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-14 12:04:20 +0100

    Initial commit - MVP complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1> {{ chartOptions.chart.title }} </h1>
+    <GChart
+      type="ColumnChart"
+      :data="energy_mix"
+      :options="chartOptions"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+  data: function () {
+    return {
+      energy_mix: [],
+      chartOptions: {
+        chart: {
+          title: 'Energy Mix',
+          subtitle: 'Yay!',
+        }
+      }
+    }
+  
+  },
+  mounted: function () {
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(res => res.json())
+    .then(generation => {
+      const origArray = generation.data.generationmix;
+      let result = [];
+      for (const object of origArray) {
+         let array = [object['fuel'], object['perc']];
+         result.push(array);
+      }
+      this.energy_mix = result;
+      this.energy_mix.unshift(['Fuel', 'Percentage']);
+    });
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
